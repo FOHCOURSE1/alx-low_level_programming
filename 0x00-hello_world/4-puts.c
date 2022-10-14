956435e4608142b816de1c50aa251a8a49a86981
FOHCOURSE1 <oketaemmanuel2000@gmail.com>
2022-10-13 19:53:52 -0700

    Task 4-6, 101,102

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
  *  *main- Serves as the entry point for the program
  *   *
  *    *Return: By default returns zero if no errors
- *      */
+ *    */
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");

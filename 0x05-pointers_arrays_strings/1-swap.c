f8583d4b1330c2c96e9881227bc4a7651ce4c70b
FOHCOURSE1 <oketaemmanuel2000@gmail.com>
2022-10-25 17:19:26 -0700

    Task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,12 +3,13 @@
  * swap_int - swaps the value of int a and int b
  *
  * @a: first int
- * @a: second int
+ * @b: second int
  * Return 0
  */
 void swap_int(int *a, int *b)
 {
 	int s;
+
 	s = *a;
 	*a = *b;
 	*b = s;
